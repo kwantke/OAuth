bc16cf43017af51e48bec7cdd5801be89fcdbe87
kokk <kokk7310@gmail.com>
2023-08-13 22:58:43 +0900

    spring validation 추가 및 적용

---
+++ src/main/java/kr/co/oauth/member/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package kr.co.oauth.member.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+
+public class LoginRequestDto {
+
+  @NotBlank(message="이메일을 입력해 주세요")
+  private String email;
+  @NotBlank(message="비밀번호를 입력해주세요")
+  private String password;
+}
