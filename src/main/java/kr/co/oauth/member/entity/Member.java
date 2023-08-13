6f8ad533a2cdcdef7af0de4419ae062c0f10e25a
kokk <kokk7310@gmail.com>
2023-08-13 23:02:04 +0900

    common entity 적용

--- src/main/java/kr/co/oauth/member/entity/Member.java
+++ src/main/java/kr/co/oauth/member/entity/Member.java
@@ -1,5 +1,6 @@
 package kr.co.oauth.member.entity;
 
+import kr.co.oauth.common.entity.Timestamped;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,34 +12,38 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @Table(name = "Member3")
-public class Member {
+public class Member extends Timestamped {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(name= "email", nullable=false)
   private String email;
   private String password;
 
   private String name;
   private String nickname;
+  private String myImgUrl;
   private String provider;
 
   private String refreshToken;
 
   @Builder
-  public Member(String email, String password,String name, String nickname, String provider, String refreshToken) {
+  public Member(String email, String password,String name, String nickname, String myImgUrl, String provider, String refreshToken) {
     this.email = email;
     this.password = password;
     this.name = name;
     this.nickname = nickname;
+    this.myImgUrl = myImgUrl;
     this.provider = provider;
     this.refreshToken = refreshToken;
   }
 
-  public Member update(String name, String email) {
+  public Member update(String name, String email, String refreshToken) {
     this.name = name;
     this.email = email;
+    this.refreshToken = refreshToken;
     return this;
   }
 }
